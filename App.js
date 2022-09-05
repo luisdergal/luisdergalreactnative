2639b80878b84dbee0e00ef6b5a48c2ce8d930af
luisdergal <luisandresdergal@gmail.com>
2022-09-04 23:20:21 -0500

    creacion de proyecto

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola, Coder! uwu</Text>
+      <Text>Luis Andres Dergal Almaraz.</Text>
       <StatusBar style="auto" />
     </View>
   );
